5395e5dbb74b237338a136dbca3d1664f0964e84
crybaby05 <benthiam750@gmail.com>
2026-07-11 22:28:32 +0000

    Ajout du service d'upload d'images vers Cloudinary

---
+++ js/Services/cloudinaryService.js
@@ -0,0 +1,24 @@
+import { CLOUDINARY_UPLOAD_URL, CLOUDINARY_CONFIG } from "../Config/cloudinary.config.js";
+
+/**
+ * Upload un fichier image vers Cloudinary (upload non signé).
+ * @param {File} file
+ * @returns {Promise<string>} l'URL sécurisée de l'image uploadée
+ */
+export async function uploadImageCloudinary(file) {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", CLOUDINARY_CONFIG.uploadPreset);
+
+    const response = await fetch(CLOUDINARY_UPLOAD_URL, {
+        method: "POST",
+        body: formData,
+    });
+
+    if (!response.ok) {
+        throw new Error("Impossible d'envoyer la photo sur Cloudinary.");
+    }
+
+    const data = await response.json();
+    return data.secure_url;
+}
