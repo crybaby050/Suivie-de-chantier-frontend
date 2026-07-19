10c9f045ccc00d004ec091bf7f1f678feccc2105
crybaby05 <benthiam750@gmail.com>
2026-07-19 14:44:41 +0000

    Ajout de l'identifiant du projet lors de l'ouverture du formulaire de tâche pour une gestion améliorée des affectations.

--- js/Pages/projets/projetDetail.js
+++ js/Pages/projets/projetDetail.js
@@ -339,7 +339,7 @@ export async function renderProjetDetail(projetId) {
         // Nouvelle tâche (une phase à la fois)
         document.querySelectorAll(".btn-add-tache").forEach(btn => {
             btn.addEventListener("click", () => {
-                openTacheForm(btn.dataset.phaseId, reload);
+                openTacheForm(btn.dataset.phaseId, projetId, reload);
             });
         });
 
